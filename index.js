5baffd6b23a68830d111b78ed76a4cb05c6e302f
mesmith454 <71784488+mesmith454@users.noreply.github.com>
2021-04-26 08:45:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendDriver = name => drivers.push(name);
+
+const destructivelyPrependDriver = name => drivers.unshift(name);
+
+const destructivelyRemoveLastDriver = name => drivers.pop();
+
+const destructivelyRemoveFirstDriver = name => drivers.shift();
+
+const appendDriver = name => [...drivers, name];
+
+const prependDriver = name => [name, ...drivers];
+
+const removeLastDriver = () => drivers.slice(0, -1);
+
+const removeFirstDriver = () => drivers.slice(1);
